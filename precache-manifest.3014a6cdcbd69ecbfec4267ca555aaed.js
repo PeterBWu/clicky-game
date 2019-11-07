6fdf4d0db645437bf22305250a26666da3c2966d
Peter Wu <23109224+PeterBWu@users.noreply.github.com>
2019-11-07 10:04:38 -0800

    Updates

---
+++ precache-manifest.3014a6cdcbd69ecbfec4267ca555aaed.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be1091b08cff18d8f70f78459e5f43d7",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "647c995969b8bc6665c5",
+    "url": "/clicky-game/static/css/main.d1820f99.chunk.css"
+  },
+  {
+    "revision": "858ac50a9a4113a4bfe7",
+    "url": "/clicky-game/static/js/2.931d97b2.chunk.js"
+  },
+  {
+    "revision": "647c995969b8bc6665c5",
+    "url": "/clicky-game/static/js/main.9d7f4ff1.chunk.js"
+  },
+  {
+    "revision": "9e63f44ec9a3dfc4c648",
+    "url": "/clicky-game/static/js/runtime-main.742e30a5.js"
+  }
+]);
